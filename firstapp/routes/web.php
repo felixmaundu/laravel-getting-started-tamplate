78f2f24337482afd4f3e6f99b086372f6b025e2a
felixmaunduhub <fmaundu10@gmail.com>
2024-05-17 22:48:11 +0300

    first commit

---
+++ firstapp/routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BlogController;
+// use App\Http\Controllers\Controller;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+// Route::get('/blog', function () {
+//     return 'index';
+// });
+
+Route::get('blogs',[BlogController::class,'index']);
+
